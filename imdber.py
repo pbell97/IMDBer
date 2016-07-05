6f3bbdc13c3e55fca039f989be7929a8364e54b9
Patrick <patrickb97@hotmail.com>
2016-07-05 13:56:51 -0500

    Add files via upload
    NEEDS WORK:
    Won't post runtime
    Overwrites all other movies with same number

--- imdber.py
+++ imdber.py
@@ -1,5 +1,6 @@
 import urllib2
 import Tkinter
+import csv
 do1 = False
 movieEdits = open('movieEdits.txt', 'a')
 
@@ -52,7 +53,7 @@ def getRating():
 
 #Getting Run Time
 def getRuntime():
-	global do1; global f; global movieEdits; global title; global date; global rating;
+	global do1; global f; global movieEdits; global title; global date; global rating; global runtime;
 	f.seek(0)
 	for line in f:
 		if do1 == True:
@@ -66,9 +67,28 @@ def getRuntime():
 	textToWrite = ("\n%s %s %s %s\n" % (title, date, rating, runtime))
 	print textToWrite
 	movieEdits.write(textToWrite)
-	
 	f.close()
 
+def writeDVD():
+	global title; global date; global rating; global runtime;
+	DVDnum = field2.get(); field2.delete(0, len(DVDnum));
+	xin = open('DVDMOVIES.CSV', 'rb')
+	dvdin = csv.reader(xin)
+	tempList = []			
+	for row in dvdin:			#Stores entire DVDMOVIES to tempList, then closes
+		tempList.append(row)
+	xin.close()
+	yout = open('DVDMOVIES.CSV', 'wb')		#Re-opens DVDMOVIES but in write mode
+	dvdout = csv.writer(yout)
+	for row in tempList:					#Rewrites DVDMOVIES, but makes edits
+		if row[1] == DVDnum:
+			row[0] = title
+			row[2] = date
+			row[3] = rating
+			row[4] = runtime
+		dvdout.writerow(row)
+	yout.close()
+	
 #Supply URL
 def getURL():
 	global f;
@@ -77,7 +97,10 @@ def getURL():
 	myText = urllib2.urlopen(myurl).read()
 	f = open('newSite.txt', 'w+')
 	f.write(myText)	
-	getTitle();getDate();getRating();getRuntime()
+	getTitle();getDate();getRating();getRuntime();writeDVD();
+	
+	
+	
 	
 #Supply URL v2
 def getURL2():
@@ -88,15 +111,16 @@ def getURL2():
 	myText = urllib2.urlopen(myurl).read()
 	f = open('newSite.txt', 'w+')
 	f.write(myText)	
-	getTitle();getDate();getRating();getRuntime()	
+	getTitle();getDate();getRating();getRuntime();writeDVD();
 	
 
 
 window = Tkinter.Tk()
 label1 = Tkinter.Label(text = "Please enter the IMDB URL"); label1.pack(side = 'top', pady = 8)
-button2 = Tkinter.Button(text = "Copied URL", command = getURL2); button2.pack(side = 'right',padx = 5, pady = 5)
-field = Tkinter.Entry(); field.pack(side = 'right',padx = 5, pady = 5)
-button1 = Tkinter.Button(text = "Sumbit URL", command = getURL); button1.pack(side = 'left',padx = 5, pady = 5)
+button2 = Tkinter.Button(text = "Copied URL", command = getURL2); button2.pack(side = 'bottom',padx = 5, pady = 5)
+field = Tkinter.Entry(); field.pack(side = 'left',padx = 5, pady = 5)
+field2 = Tkinter.Entry(); field2.pack(side = 'right', padx = 10, pady = 10, )
+button1 = Tkinter.Button(text = "Sumbit URL", command = getURL); button1.pack(side = 'bottom',padx = 5, pady = 5)
 window.mainloop()
 
 movieEdits.close()
